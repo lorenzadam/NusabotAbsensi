40719261051db6689e86fe1c82c5833877800038
Adam Damara <lorenzrenz@gmail.com>
2021-07-02 00:52:59 +0700

    Pemotongan template init

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="id">
+<?php include 'header.php' ?>
+<body class="hold-transition sidebar-mini layout-fixed" data-panel-auto-height-mode="height">
+<div class="wrapper">
+
+  <!-- Navbar -->
+  <?php include 'top.php' ?>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/logo-white.png" alt="Nusabot Logo" width="30%">
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="info">
+          <a href="#" class="d-block">Bapak Budi</a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Cari" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <?php include 'sidemenu.php' ?>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper iframe-mode" data-widget="iframe" data-loading-screen="750">
+    <div class="nav navbar navbar-expand navbar-white navbar-light border-bottom p-0">
+      <div class="nav-item dropdown">
+        <a class="nav-link bg-danger dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Tutup</a>
+        <div class="dropdown-menu mt-0">
+          <a class="dropdown-item" href="#" data-widget="iframe-close" data-type="all">Tutup Semua</a>
+          <a class="dropdown-item" href="#" data-widget="iframe-close" data-type="all-other">Tutup Yang Lainnya</a>
+        </div>
+      </div>
+      <a class="nav-link bg-light" href="#" data-widget="iframe-scrollleft"><i class="fas fa-angle-double-left"></i></a>
+      <ul class="navbar-nav overflow-hidden" role="tablist"></ul>
+      <a class="nav-link bg-light" href="#" data-widget="iframe-scrollright"><i class="fas fa-angle-double-right"></i></a>
+      <a class="nav-link bg-light" href="#" data-widget="iframe-fullscreen"><i class="fas fa-expand"></i></a>
+    </div>
+    <div class="tab-content">
+      <div class="tab-empty">
+        <h2 class="display-4">Selamat Datang :)</h2>
+      </div>
+      <div class="tab-loading">
+        <div>
+          <h2 class="display-4">Mohon tunggu... <i class="fa fa-sync fa-spin"></i></h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Hak Cipta &copy; 2021 <a href="https://nusabot.id">Nusabot.id</a></strong>
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Versi</b> 0.0.1
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
---
+++ sidemenu.php
@@ -0,0 +1,50 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="pages/pengguna.php" class="nav-link">
+              <i class="nav-icon fas fa-user"></i>
+              <p>Pengguna</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/absensi.php" class="nav-link">
+              <i class="nav-icon fas fa-baby"></i>
+              <p>Absensi</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/cuti.php" class="nav-link">
+              <i class="nav-icon fas fa-calendar"></i>
+              <p>Cuti</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-columns"></i>
+              <p>Pengaturan<i class="fas fa-angle-left right"></i></p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/jabatan.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Jabatan / Status</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/cabang.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Cabang / Gedung</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="pages/profil.php" class="nav-link">
+              <i class="nav-icon fas fa-box"></i>
+              <p>Profil</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
