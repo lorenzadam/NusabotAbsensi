e1c2f71d47c7e7d398d5ef3a71d4ad57ca28e6d6
Adam Damara <lorenzrenz@gmail.com>
2021-07-25 16:50:03 +0700

    Filter absen

--- pages/absensi/index.php
+++ pages/absensi/index.php
@@ -2,7 +2,41 @@
 require_once("../../etc/config.php");
 require_once("../../etc/function.php");
 
-$result = mysqli_query($mysqli, "SELECT * FROM absensi");
+if (!isset($_POST['tampilkan'])) {
+  $result = mysqli_query($mysqli, "SELECT * FROM absensi limit 500");
+  $formCabang = 1;
+} else {
+  if ($_POST['kategori'] == 0) {
+    $result = mysqli_query($mysqli, "
+      SELECT absensi.nomor_induk, pengguna.cabang_gedung, absensi.absen, absensi.kategori, absensi.idmesin
+      FROM pengguna
+      INNER JOIN absensi ON absensi.nomor_induk=pengguna.nomor_induk
+      WHERE pengguna.cabang_gedung = '{$_POST['cabang_gedung']}' AND (absensi.absen BETWEEN '{$_POST['awal']}' AND '{$_POST['akhir']}');
+      ");
+  } else {
+    $result = mysqli_query($mysqli, "
+        SELECT absensi.nomor_induk, pengguna.cabang_gedung, absensi.absen, absensi.kategori, absensi.idmesin
+        FROM pengguna
+        INNER JOIN absensi ON absensi.nomor_induk=pengguna.nomor_induk
+        WHERE pengguna.cabang_gedung = '{$_POST['cabang_gedung']}' AND absensi.kategori = '{$_POST['kategori']}' AND (absensi.absen BETWEEN '{$_POST['awal']}' AND '{$_POST['akhir']}');
+        ");
+  }
+  if ($_POST['kategori'] == 0) {
+    $formKategori = "Semua Kategori";
+  } else {
+    if ($_POST['kategori'] == 1) {
+      $formKategori = "Masuk";
+    } elseif ($_POST['kategori'] == 2) {
+      $FormKategori = "Mulai Istirahat";
+    } elseif ($_POST['kategori'] == 3) {
+      $formKategori = "Selesai Istirahat";
+    } elseif ($_POST['kategori'] == 4) {
+      $formKategori = "Pulang";
+    }
+  }
+  $valueKategori = $_POST['kategori'];
+  $formCabang = $_POST['cabang_gedung'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -45,6 +79,41 @@ $result = mysqli_query($mysqli, "SELECT * FROM absensi");
           <div class="row">
             <div class="col-12">
               <div class="card">
+                <div class="card-header">
+                  <form method="post" action="">
+                    <div class="form-group">
+                      <div class="form-group">
+                        <?php //comboBoxSelect($mysqli, "cabang_gedung", "id", "lokasi", "cabang_gedung", 0) 
+                        ?>
+                        <?php comboBoxSelectEdit($mysqli, "cabang_gedung", "id", "lokasi", "cabang_gedung", 0, $formCabang, getAnyTampil($mysqli, "lokasi", "cabang_gedung", "id", $formCabang)) ?>
+                      </div>
+                      <div class="form-group">
+                        <div class="row">
+                          <div class="col-3">
+                            <input type="date" name="awal" value="<?php echo $_POST['awal'] ?>" class="form-control">
+                          </div>
+                          sampai
+                          <div class="col-3">
+                            <input type="date" name="akhir" value="<?php echo $_POST['akhir'] ?>" class="form-control">
+                          </div>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <select class="custom-select" name="kategori">
+                          <?php if (isset($_POST['kategori'])) { ?>
+                            <option value="<?php echo $valueKategori ?>"><?php echo $formKategori ?></option>
+                          <?php } ?>
+                          <option value="0">Semua Kategori</option>
+                          <option value="1">Masuk</option>
+                          <option value="2">Istirahat Mulai</option>
+                          <option value="3">Istirahat Selesai</option>
+                          <option value="4">Pulang</option>
+                        </select>
+                      </div>
+                    </div>
+                    <button type="submit" name="tampilkan" class="btn btn-success">Tampilkan</button>
+                  </form>
+                </div>
                 <!-- /.card-header -->
                 <div class="card-body">
                   <table id="example1" class="table table-bordered table-striped">
@@ -75,21 +144,24 @@ $result = mysqli_query($mysqli, "SELECT * FROM absensi");
                           continue;
                         }
 
-                        $cabang = getAnyTampil($mysqli, "lokasi", "cabang_gedung", "id", getAnyTampil($mysqli,"cabang_gedung","pengguna","nomor_induk",$data['nomor_induk']));
-                        $zona = getAnyTampil($mysqli, "zona_waktu", "cabang_gedung", "id", getAnyTampil($mysqli,"cabang_gedung","pengguna","nomor_induk",$data['nomor_induk']));
+                        $cabang = getAnyTampil($mysqli, "lokasi", "cabang_gedung", "id", getAnyTampil($mysqli, "cabang_gedung", "pengguna", "nomor_induk", $data['nomor_induk']));
+                        $zona = getAnyTampil($mysqli, "zona_waktu", "cabang_gedung", "id", getAnyTampil($mysqli, "cabang_gedung", "pengguna", "nomor_induk", $data['nomor_induk']));
 
-                        if($zona == 1){
+                        if ($zona == 1) {
                           $seconds = "+25200 seconds";
-                        } else if($zona == 2){
+                          $zonaWaktu = "WIB";
+                        } else if ($zona == 2) {
                           $seconds = "+28800 seconds";
+                          $zonaWaktu = "WITA";
                         } else {
                           $seconds = "+32400 seconds";
+                          $zonaWaktu = "WIT";
                         }
 
                         $startTime = date($data['absen']);
                       ?>
                         <tr>
-                          <td><?php echo date('Y-m-d H:i:s',strtotime($seconds,strtotime($startTime))) ?></td>
+                          <td><?php echo date('Y-m-d H:i:s', strtotime($seconds, strtotime($startTime))) . " " . $zonaWaktu ?></td>
                           <td><?php echo getAnyTampil($mysqli, 'nama', 'pengguna', 'nomor_induk', $data['nomor_induk']) ?></td>
                           <td><?php echo $data['nomor_induk'] ?></td>
                           <td><?php echo $cabang ?></td>
