882bc55e45c071c47971c48bc484490195a88d0d
Lorenz Adam Damara <lorenzrenz@gmail.com>
2024-11-07 09:53:47 +0700

    add option fo error reporting

--- etc/config.php
+++ etc/config.php
@@ -3,6 +3,7 @@
  * using mysqli_connect for database connection
  */
 
+// ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 error_reporting(E_ERROR | E_PARSE);
  
 $databaseHost = 'localhost';
