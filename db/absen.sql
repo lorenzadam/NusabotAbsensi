5439c189aee32e828a2529530b860ba2652473a2
Lorenz Adam Damara <lorenzrenz@gmail.com>
2024-11-15 11:34:38 +0700

    add db scheme

--- db/absen.sql
+++ db/absen.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 06 Agu 2021 pada 07.47
--- Versi server: 10.4.19-MariaDB
--- Versi PHP: 8.0.7
+-- Waktu pembuatan: 15 Nov 2024 pada 05.34
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `absensi_baru`
+-- Database: `absen`
 --
 
 -- --------------------------------------------------------
@@ -34,7 +34,18 @@ CREATE TABLE `absensi` (
   `absen_maks` datetime NOT NULL,
   `kategori` char(1) DEFAULT NULL COMMENT '1=jam_masuk, 2=istirahat_mulai, 3=istirahat_selesai, 4=pulang',
   `idmesin` char(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `absensi`
+--
+
+INSERT INTO `absensi` (`id`, `nomor_induk`, `absen`, `absen_maks`, `kategori`, `idmesin`) VALUES
+(49, '12228402', '2024-11-13 08:26:17', '2024-11-13 16:26:17', '1', '1'),
+(50, '12228418', '2024-11-13 08:26:59', '2024-11-13 16:26:59', '', '1'),
+(51, '12228418', '2024-11-14 02:27:36', '2024-11-14 10:27:36', '', '1'),
+(52, '12228402', '2024-11-14 03:34:30', '2024-11-14 11:34:30', '1', '1'),
+(53, '12228402', '2024-11-14 03:34:53', '2024-11-14 11:34:53', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -52,14 +63,15 @@ CREATE TABLE `cabang_gedung` (
   `hari_libur` char(15) NOT NULL,
   `zona_waktu` char(1) NOT NULL,
   `aktif` char(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `cabang_gedung`
 --
 
 INSERT INTO `cabang_gedung` (`id`, `lokasi`, `jam_masuk`, `jam_pulang`, `istirahat_mulai`, `istirahat_selesai`, `hari_libur`, `zona_waktu`, `aktif`) VALUES
-(0, 'mainland', '00:00:00', '00:00:00', '00:00:00', '00:00:00', '1,2,3,4,5,6,7', '1', '1');
+(0, 'mainland', '00:00:00', '00:00:00', '00:00:00', '00:00:00', '1,2,3,4,5,6,7', '1', '1'),
+(1, 'penggung', '00:00:00', '08:00:00', '02:35:00', '03:36:00', '0,6', '1', '1');
 
 -- --------------------------------------------------------
 
@@ -71,7 +83,28 @@ CREATE TABLE `cuti` (
   `id` int(11) NOT NULL,
   `nomor_induk` char(30) NOT NULL,
   `tanggal` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `denda`
+--
+
+CREATE TABLE `denda` (
+  `id` int(11) NOT NULL,
+  `per_menit` int(1) NOT NULL,
+  `rupiah_pertama` decimal(10,0) NOT NULL,
+  `rupiah_selanjutnya` decimal(10,0) NOT NULL,
+  `prioritas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `denda`
+--
+
+INSERT INTO `denda` (`id`, `per_menit`, `rupiah_pertama`, `rupiah_selanjutnya`, `prioritas`) VALUES
+(1, 1, 10000, 1000, 1);
 
 -- --------------------------------------------------------
 
@@ -82,7 +115,7 @@ CREATE TABLE `cuti` (
 CREATE TABLE `hak_akses` (
   `id` int(11) NOT NULL,
   `hak` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `hak_akses`
@@ -104,7 +137,7 @@ CREATE TABLE `jabatan_status` (
   `jabatan_status` varchar(15) NOT NULL,
   `hak_akses` char(1) NOT NULL,
   `aktif` char(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `jabatan_status`
@@ -126,7 +159,7 @@ CREATE TABLE `libur_khusus` (
   `id` int(11) NOT NULL,
   `tanggal` date NOT NULL,
   `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -142,14 +175,19 @@ CREATE TABLE `pengguna` (
   `cabang_gedung` char(1) NOT NULL,
   `password` text NOT NULL,
   `aktif` char(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `pengguna`
 --
 
 INSERT INTO `pengguna` (`nomor_induk`, `nama`, `tag`, `jabatan_status`, `cabang_gedung`, `password`, `aktif`) VALUES
-('0', 'Nusabot.id', '', '0', '0', '2dcca9cf0a56e9f1cd2141d6df458f62', '1');
+('0', 'Nusabot.id', '', '0', '0', '2dcca9cf0a56e9f1cd2141d6df458f62', '1'),
+('1', 'abin', 'abin', '5', '1', 'c4ca4238a0b923820dcc509a6f75849b', '0'),
+('12228402', 'Yan', '3577438329', '2', '1', 'c3a92251d8c64ca74ba88069d7cc6b2c', '1'),
+('12228418', 'fifi', '3069959139', '2', '1', '2a372a408d5d7f2ddc30142b9fdc2563', '1'),
+('2', 'ijal', '2863188851', '3', '1', 'c81e728d9d4c2f636f067f89cc14862c', '1'),
+('5', 'ewing', '0241136899', '2', '1', 'e4da3b7fbbce2345d7772b0674a318d5', '1');
 
 --
 -- Indexes for dumped tables
@@ -171,6 +209,12 @@ ALTER TABLE `cabang_gedung`
 -- Indeks untuk tabel `cuti`
 --
 ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `denda`
+--
+ALTER TABLE `denda`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -205,19 +249,25 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT untuk tabel `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT untuk tabel `cabang_gedung`
 --
 ALTER TABLE `cabang_gedung`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `cuti`
 --
 ALTER TABLE `cuti`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `denda`
+--
+ALTER TABLE `denda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `jabatan_status`
